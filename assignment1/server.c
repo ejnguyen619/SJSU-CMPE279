20c43298019c06b8d8eaaaa1b74f0500a53a1925
Edward Nguyen <edwardn2147@yahoo.com>
2020-05-04 21:58:18 -0700

    Add privilege separation

--- assignment1/server.c
+++ assignment1/server.c
@@ -13,7 +13,8 @@ int main(int argc, char const *argv[])
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+    char *hello = "Hello from server";
+    uid_t id = 65534;	
        
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
@@ -39,7 +40,7 @@ int main(int argc, char const *argv[])
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
-    } 
+    }
     if (listen(server_fd, 3) < 0) 
     { 
         perror("listen"); 
@@ -50,10 +51,41 @@ int main(int argc, char const *argv[])
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
-    } 
-    valread = read( new_socket , buffer, 1024); 
-    printf("%s\n",buffer ); 
-    send(new_socket , hello , strlen(hello) , 0 ); 
-    printf("Hello message sent\n"); 
-    return 0; 
+    }
+
+	// Privilege Separation
+	pid_t fid = fork();
+	//printf("Debug: fork id is %d.\n",fid);
+	
+	// Child Process
+	if (fid == 0){
+	   printf("-------------------------------\n");
+	   printf("Child process in progress\n");
+		
+	   // Drop privilege to "nobody" user
+	   setuid(id);
+	   //printf("Debug: child real ID is %d\n", getuid());
+	   printf("Child process privilege dropped\n");
+	   printf("-------------------------------\n");
+
+       // Process data from client
+	   valread = read( new_socket , buffer, 1024); 
+	   printf("%s\n",buffer ); 
+	   send(new_socket , hello , strlen(hello) , 0 ); 
+	   printf("Hello message sent\n"); 
+
+	   printf("-------------------------------\n");		
+	   printf("Child process complete\n");
+	}
+	// Parent Process
+	else{
+	   int status = 0;
+	   // Wait for child process to exit before proceeding
+	   while ((wait(&status) > 0));
+	   printf("Parent process complete\n");
+	}
+	
+	//printf("Exiting program\n");
+    return 0;
+	
 } 
